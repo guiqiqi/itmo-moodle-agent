9fccea0090b61aac368fdb8c3601c2346dfe9065
guiqiqi <guiqiqi187@gmail.com>
2025-12-13 13:26:36 +0300

    feat: add fastapi app and api router

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,13 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.src.config import settings
+from src.database import init as init_db
+from src.api import init as init_api
+
 
 import logging
+from contextlib import asynccontextmanager
 
 
 logger = logging.getLogger()
@@ -10,11 +17,30 @@ for handler in settings.LOG_HANDLERS:
     logger.addHandler(handler)
 
 
-class BackendException(Exception):
-    _base_code: int = 0
-    _code: int
-    msg: str
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+    # NOTE: shutdown handler (if any) goes here
 
-    @property
-    def code(self) -> int:
-        return self._code + self._base_code
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    lifespan=lifespan,
+    version=settings.API_VERSION,
+    docs_url=f"/api/{settings.API_VERSION}/docs",
+    openapi_url=f"/api/{settings.API_VERSION}/openapi.json"
+)
+
+app.include_router(init_api(), prefix="/api")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        origin.rstrip("/") for origin in settings.CORS_ORIGINS
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
